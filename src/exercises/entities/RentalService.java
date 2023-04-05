f12d5807ce0f1e1815ec40a48bf91d36566bdb6e
Vitor Vianna <vitormatheusfv@gmail.com>
2023-04-05 16:37:13 -0300

    Exercise CarRental finished

--- src/exercises/entities/RentalService.java
+++ src/exercises/entities/RentalService.java
@@ -1,5 +1,8 @@
 package exercises.entities;
 
+import javax.xml.transform.stax.StAXResult;
+import java.time.Duration;
+
 public class RentalService {
     private Double pricePerHour;
     private Double pricePerDay;
@@ -13,7 +16,21 @@ public class RentalService {
     }
 
     public void processInvoice(CarRental carRental) {
-        carRental.setInvoice(new Invoice());
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
+
+        double basicPayment;
+
+        if (hours <= 12.0) {
+            basicPayment = pricePerDay * Math.ceil(hours);
+        } else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
     }
 
     public Double getPricePerHour() {
